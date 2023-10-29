c47b6086ea846d2422b871aedd99793082e09665
nikkiguo <nikkiguo1@gmail.com>
2023-10-29 11:40:20 -0400

    start arrays strings ctci problems

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ An archive of problems solved
 | # | Problem | Solution | Difficulty | Source | Date Completed |
 |---| ----- | -------- | ---------- | -------------- | -------------- |
 | 20 | [Valid Parentheses](https://leetcode.com/problems/valid-parentheses/) | [Python](./Solutions/LeetCode/Stack/ValidParentheses.py) | Easy | LeetCode | 27/10/2023 |
+| 1.1 | Is Unique | [Python](./Solutions/CTCI/ArraysStrings/isUnique.py) | Easy | CTCI | 29/10/2023 |
--- Solutions/CTCI/ArraysStrings/isUnique.py
+++ Solutions/CTCI/ArraysStrings/isUnique.py
@@ -1,5 +1,34 @@
 """
 1.1 Is Unique: 
-Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures? 
+Implement an algorithm to determine if a string has all unique characters. 
 """
 
+def isUnique(s):
+    """
+    :type s: str
+    :rtype: bool
+    """
+    letterDict = {}
+    for l in s:
+        if letterDict.get(l):
+            return False
+        else:
+            letterDict[l] = 1
+    return True
+
+"""
+1.1 Is Unique: 
+What if you cannot use additional data structures? 
+"""
+def isUnique(s):
+    """
+    :type s: str
+    :rtype: bool
+    """
+    s = list(s)
+    s.sort()
+
+    for i in range(len(s) - 1):
+        if s[i] == s[i + 1]:
+            return False
+    return True
